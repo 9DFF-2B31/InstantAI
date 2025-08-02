634270b80055374205fc942e6dcaffb3275a9957
9DFF-2B31 <136964344+9DFF-2B31@users.noreply.github.com>
2025-08-02 22:43:27 +0530

    frontend-complete

--- client/src/pages/RemoveBackground.jsx
+++ client/src/pages/RemoveBackground.jsx
@@ -3,11 +3,11 @@ import React, { useState } from 'react'
 
 const RemoveBackground = () => {
 
-    const [input, setInput] = useState('')
-    const onSubmitHandler = async (e) => {
-      e.preventDefault();
-    }
- return (
+  const [input, setInput] = useState('')
+  const onSubmitHandler = async (e) => {
+    e.preventDefault();
+  }
+  return (
     <div>
       <div className='h-full overflow-y-scroll p-6 flex items-start flex-wrap gap-4 text-slate-700
 '>
@@ -19,8 +19,8 @@ const RemoveBackground = () => {
             <h1 className='text-xl font-semibold'>Background Removal</h1>
           </div>
           <p className='mt-6 text-sm font-medium'>Upload image</p>
-          
-          <input onChange={(e) => setInput(e.target.files[0])} type="file"  accept="image/*" className='w-full p-2 px-3 mt-2 outline-none text-sm rounded-md border border-gray-300 text-gray-600'  required />
+
+          <input onChange={(e) => setInput(e.target.files[0])} type="file" accept="image/*" className='w-full p-2 px-3 mt-2 outline-none text-sm rounded-md border border-gray-300 text-gray-600' required />
           <p>Supports JPG, PNG, snd other image formats</p>
           <button className='w-full flex justify-center items-center gap-2 bg-gradient-to-r from-[#F6AB41] to-[#FF4938] text-white px-4 py-2 mt-6 text-sm rounded-lg cursor-pointer
 '>
