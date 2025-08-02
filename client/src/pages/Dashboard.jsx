92d191a5e0c18183ad5ac156ea0efb2c46a6a44f
9DFF-2B31 <136964344+9DFF-2B31@users.noreply.github.com>
2025-08-02 21:41:58 +0530

    pages

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,20 +1,63 @@
 import React, { useEffect, useState } from 'react'
 import { dummyCreationData } from '../assets/assets'
+import { Gem, Sparkles } from 'lucide-react'
+import { Protect } from '@clerk/clerk-react'
+import CreationItem from '../components/CreationItem'
 
 const Dashboard = () => {
-  const [creations,setCreations] =useState([])
-  const getDashboardData=async()=>{
+  const [creations, setCreations] = useState([])
+  const getDashboardData = async () => {
     setCreations(dummyCreationData)
 
   }
-  useEffect(()=>{
+  useEffect(() => {
     getDashboardData()
-  },[])
+  }, [])
 
 
   return (
-    <div>
-      
+    <div className='h-full overflow-y-scroll p-6'>
+      <div className='flex justify-start gap-4 flex-wrap'>
+        {/* Total Creations Card */}
+        <div className='flex justify-between items-center w-72 p-4 px-6 bg-white rounded-xl border border-gray-200'>
+          <div className='text-slate-600'>
+            <p className='text-sm'>Total Creations</p>
+            <h2 className='text-xl font-semibold'>{creations.length}</h2>
+          </div>
+          <div className='w-10 h-10 rounded-lg bg-gradient-to-br from-[#3588F2] to-[#0BB007] text-white flex justify-center items-center'>
+            <Sparkles className='w-5 text-white' />
+          </div>
+        </div>
+
+        {/* //active plan card */}
+        <div className='flex justify-between items-center w-72 p-4 px-6 bg-white rounded-xl border border-gray-200'>
+          <div className='text-slate-600'>
+            <p className='text-sm'>Plan Status</p>
+            <h2 className='text-xl font-semibold'><Protect plan="premium" fallback="free">Premium</Protect></h2>
+          </div>
+          <div className='w-10 h-10 rounded-lg bg-gradient-to-br from-[#FF61C5] to-[#9E53EE] text-white flex justify-center items-center'>
+            <Gem className='w-5 text-white' />
+          </div>
+        </div>
+
+
+
+      </div>
+
+      <div className='space-y-3'>
+        <p className='mt-6 mb-4'>Recent Creations</p>
+
+        {
+          creations.map((item) => <CreationItem key={CreationItem.id} item={item} />)
+        }
+        <div>
+
+        </div>
+
+
+
+
+      </div>
     </div>
   )
 }
